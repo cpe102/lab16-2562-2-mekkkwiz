f6fdd899573895b3582e21a5625a910740a9c95c
mek <themakza0021@gmail.com>
2020-03-03 16:43:03 +0700

    done

--- lab16_3.cpp
+++ lab16_3.cpp
@@ -22,3 +22,43 @@ int main(){
 }
 
 //Write Definition of randomVector() showVector() and dotProduct() here
+
+vector<int> randomVector(int N)
+{
+	vector<int> vec1(N);
+	vector<int> vec2(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		vec1[i] = rand()%10;
+	}
+	for (int j = 0; j < N; j++)
+	{
+		vec2[j] = rand()%10;
+	}
+
+	return vec1,vec2; 
+}
+
+
+void showVector(vector<int> q)
+{
+	cout << "[ ";
+	for (int i = 0; i < q.size(); i++)
+	{
+		cout << q[i] << " ";
+	}
+	cout << "]";
+}
+
+
+int dotProduct(vector<int> x,vector<int> y)
+{
+	int sum = 0;
+	for (int i = 0; i < x.size(); i++)
+	{	
+		sum += x[i]*y[i];	
+	}
+	return sum;
+}
+
